430ac5bba4f624a1799cb203dd0e357a527c04ef
le50 <leti.tec1000@gmail.com>
2025-10-03 10:47:29 -0300

    Adicionado objetos

--- src/br/com/etec/biblioteca/cadastro/ClassePrincipal.java
+++ src/br/com/etec/biblioteca/cadastro/ClassePrincipal.java
@@ -3,8 +3,13 @@ package br.com.etec.biblioteca.cadastro;
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ClasseInformacoes Info_obj = new ClasseInformacoes();
+		ClasseInformacoes Info_obj2 = new ClasseInformacoes("Dom Casmurro", "Machado de Assis");
+		
+		Info_obj.mostrardados(); // null 0
 
+		Info_obj2.mostrardados(); // null 0 autor e titulo
 	}
 
 }
